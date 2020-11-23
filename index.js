6ad6b1c427c93cb78d849a987e982e59b4522021
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-11-23 19:08:49 +0530

    updated to dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var express = require('express')
-var app = express()
-app.get('/', function (req, res) {
-    res.send('GET request to homepage')
-  })
-app.listen(3000)
+var express = require("express");
+var app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", function (req, res) {
+  res.send("GET request to homepage");
+});
+
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
