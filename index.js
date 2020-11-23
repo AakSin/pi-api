cc0c5bc0f8f626593f458b019cd3d91f55fd322e
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-11-23 19:51:24 +0530

    set up python endpoint

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 var express = require("express");
 var app = express();
+var fs = require("fs");
+const { spawn } = require("child_process");
+
 const PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
   res.send("GET request to homepage");
 });
-app.get("/:code",function(req, res){
-    res.send(req.params.code);
+app.get("/:code", function (req, res) {
+  fs.writeFile("temp.py", req.params.code, function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+  var dataToSend;
+  // spawn new child process to call the python script
+  const python = spawn("python", ["temp.py"]);
+  // collect data from script
+  python.stdout.on("data", function (data) {
+    console.log("Pipe data from python script ...");
+    dataToSend = data.toString();
+  });
+  // in close event we are sure that stream from child process is closed
+  python.on("close", (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    // send data to browser
+    res.send(dataToSend);
+  });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
