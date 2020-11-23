bb0692510acbf4120b3404f62b4463259d8b3e2a
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-11-23 19:31:55 +0530

    added code wildcard

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3000;
 app.get("/", function (req, res) {
   res.send("GET request to homepage");
 });
+app.get("/:code",function(req, res){
+    res.send(req.params.code);
+});
+
 
 app.listen(PORT, () => {
   console.log(`Our app is running on port ${PORT}`);
